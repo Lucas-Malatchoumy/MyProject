a9237796f8c1c65e97cdb4da7d258d8be3e83bab
Lucas Malatchoumy <malatchoumylucas.com>
2023-06-30 13:49:16 +0200

    Change port

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-const express = require('express')
+const express = require('express');
+const app     = express();
+const port    = 8080;
+const square = require("./lib/square")
 
-const app = express();
-const port = 80;
+app.disable("x-powered-by")
 
 app.get('/', (req, res) => {
-        res.send("Hello world");
+    res.send("hello world");
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port : ${port}`)
+app.get('/square/:nb', (req, res) => {
+	const nb = req.params.nb
+	res.send(square(parseInt(nb)).toString());
 });
+
+let server;
+
+server = app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+module.exports = {
+    app,
+    closeServer: () => {
+        server.close();
+    }
+};
