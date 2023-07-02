eb8920b5813aa57d5c710c8aaf9a09367a6c5d76
Lucas Malatchoumy <malatchoumylucas.com>
2023-07-02 22:42:43 +0200

    add all routes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const square = require("./lib/square")
 const app     = express();
 const bodyParser = require('body-parser');
@@ -6,13 +7,26 @@ const port    = 8080;
 const sequelize = require('./db');
 const loginRoute = require('./routes/login');
 const registerRoute = require('./routes/register');
+const homeRoute = require('./routes/home')
 
 app.disable("x-powered-by")
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.use('/login', loginRoute);
 app.use('/register', registerRoute);
-app.use(express.static('public'));
+app.use('/home', homeRoute);
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.get('/', (req, res) => {
+  res.send("Hello world");
+});
+
+app.get('/square/:nb', (req, res) => {
+const nb = req.params.nb
+res.send(square(parseInt(nb)).toString());
+});
 
 sequelize
   .authenticate()
